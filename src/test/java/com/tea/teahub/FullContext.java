f19a170dc7e10d15ae2577b49db751dc48b1eb00
AlexBar17 <ab114@yandex.ru>
2026-01-20 22:58:34 +0400

    zonky

--- src/test/java/com/tea/teahub/FullContext.java
+++ src/test/java/com/tea/teahub/FullContext.java
@@ -3,16 +3,24 @@ package com.tea.teahub;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tea.teahub.mapper.TeaMapper;
 import com.tea.teahub.repository.TeaStorage;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.EMBEDDED;
+
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public abstract class FullContext extends PostgresContainerTest {
+@AutoConfigureEmbeddedDatabase(
+        provider = EMBEDDED,
+        type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES
+)
+public abstract class FullContext {
 
     @Autowired
     protected MockMvc mockMvc;
